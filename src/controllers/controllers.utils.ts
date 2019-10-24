c4f5162bc790e2fb66b759d640c81c6193e78089
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-10-24 11:32:06 -0500

    create a privateRoute middleware function

--- src/controllers/controllers.utils.ts
+++ src/controllers/controllers.utils.ts
@@ -1,7 +1,34 @@
 import * as redis from "redis";
 import * as bluebird from "bluebird";
+import { Request, Response, NextFunction } from "express";
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-export const redisClient = redis.createClient({ host: "redis" }) as any;
+export const redisClient = redis.createClient({
+  url: process.env.REDIS_URI
+}) as any;
+
+export const privateRoute = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(400).send("Not Authorized!");
+
+  const token = authorization.replace("Bearer ", "");
+
+  const userId = await redisClient.getAsync(token);
+
+  if (!userId) return res.status(400).send("Not Authorized!");
+
+  req.body = {
+    ...req.body,
+    token,
+    userId
+  };
+
+  next();
+};
